d20b0317906fa33fe3a4e48b670d51a32c525c0d
Nick Redmark <nicolamr@gmail.com>
2019-10-01 17:30:30 +0200

    simpler config

--- next.config.js
+++ next.config.js
@@ -3,8 +3,5 @@ require("dotenv").config();
 module.exports = {
   env: {
     SERVERS: process.env.SERVERS,
-    NAMESPACE: process.env.NAMESPACE,
-    STREAMS: process.env.STREAMS,
-    READONLY: process.env.READONLY
   }
 };
